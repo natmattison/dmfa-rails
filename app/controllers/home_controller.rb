9df716c3b6409d8fdbb43f01935a590828b1ea01
natmat <natmattison@gmail.com>
2016-03-22 22:09:11 -0400

    setup contact form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,6 +13,29 @@ class HomeController < ApplicationController
   end
   
   def send_email
+    require 'pony'
+    Pony.mail(
+      :from => params[:name] + "<" + params[:email] + ">",
+      :to => ENV['TO_EMAIL'],
+      :bcc => ENV['BCC_EMAIL'],
+      :subject => params[:name] + " has contacted you from debbiemattisonfineart.com",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => { 
+        :address              => 'smtp.sendgrid.net', 
+        :port                 => '587', 
+        :enable_starttls_auto => true, 
+        :user_name            => ENV['SENDGRID_USERNAME'], 
+        :password             => ENV['SENDGRID_PASSWORD'], 
+        :authentication       => :plain, 
+        :domain               => ENV['SENDGRID_DOMAIN']
+      })
+    
+    redirect_to '/contact_success'
+  end
+
+  def contact_success
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about' => 'home#about'
   get 'contact' => 'home#contact'
   post 'contact' => 'home#send_email'
+  get  'contact_success' => 'home#contact_success'
   get 'reproductions' => 'home#reproductions'
   get 'gallery' => 'paintings#index'
   get 'detail/:id' => 'paintings#show'
